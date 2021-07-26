ef2522595f3ead4a8b7798fe226d9015fd3657b7
cmmerritt <christiane.merritt@gmail.com>
2021-07-26 16:39:23 -0700

    static color block displays on page

--- src/containers/ColorRandomizer.jsx
+++ src/containers/ColorRandomizer.jsx
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
 
 export default class ColorRandomizer extends Component {
+  state = {
+    randomColor: 'red',
+  }
   render() {
-    return <h1>This could be a buttery biscuit</h1>;
+    const colors = [
+      'red',
+      'yellow',
+      'blue',
+      'purple',
+      'green',
+      'orange'
+    ];
+    return (
+      <>
+        <div
+          style = {{ 
+            backgroundColor: this.state.randomColor,
+            width: '200px',
+            height: '200px'
+          }}
+        ></div>
+      </>
+    );
   }
 }
